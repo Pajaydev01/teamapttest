66ab4c878136403a93626d9b7a2042e1d7f1632a
pajaydev01 <johnsonaniyi1@gmail.com>
2022-10-24 09:15:11 +0100

    Assignment completion

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, AfterContentChecked, AfterContentInit} from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+revealer:boolean=false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    window.addEventListener('scroll',this.reveal);
+  }
+
+
+  reveal=()=>{
+    const reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+}
